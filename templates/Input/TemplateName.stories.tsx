660d2e731bc1d4ac782ccb77b451151323ce2d16
Dima Vyshniakov <morewings@gmail.com>
2023-12-18 01:37:18 +0100

    improve input template

--- templates/Input/TemplateName.stories.tsx
+++ templates/Input/TemplateName.stories.tsx
@@ -1,5 +1,5 @@
 import type {Meta, StoryObj} from '@storybook/react';
-// import {fn} from '@storybook/test';
+import {fn} from '@storybook/test';
 
 import {TemplateName} from './TemplateName.tsx';
 
@@ -10,9 +10,87 @@ const meta = {
         // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
         layout: 'centered',
     },
-    args: {},
+    args: {
+        onClick: fn(),
+        onBlur: fn(),
+        onFocus: fn(),
+        onKeyDown: fn(),
+        onKeyUp: fn(),
+    },
     argTypes: {
+        value: {control: 'text'},
+        defaultValue: {control: 'text'},
+        onClick: {
+            table: {
+                disable: true,
+            },
+        },
+        onBlur: {
+            table: {
+                disable: true,
+            },
+        },
+        onFocus: {
+            table: {
+                disable: true,
+            },
+        },
+        onKeyDown: {
+            table: {
+                disable: true,
+            },
+        },
+        onKeyUp: {
+            table: {
+                disable: true,
+            },
+        },
+        id: {
+            table: {
+                disable: true,
+            },
+        },
+        role: {
+            table: {
+                disable: true,
+            },
+        },
+        dataAttributes: {
+            table: {
+                disable: true,
+            },
+        },
         className: {
+            table: {
+                disable: true,
+            },
+        },
+        name: {
+            table: {
+                disable: true,
+            },
+        },
+        autoFocus: {
+            table: {
+                disable: true,
+            },
+        },
+        form: {
+            table: {
+                disable: true,
+            },
+        },
+        onChange: {
+            table: {
+                disable: true,
+            },
+        },
+        validatorFn: {
+            table: {
+                disable: true,
+            },
+        },
+        prefix: {
             table: {
                 disable: true,
             },
