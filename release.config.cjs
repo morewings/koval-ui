eaa309624b7014c432b52718d39d3ebc58e3146f
Dima Vyshniakov <morewings@gmail.com>
2024-02-18 21:16:49 +0100

    fix(release): bump version number in package.json

--- release.config.cjs
+++ release.config.cjs
@@ -3,4 +3,16 @@
  */
 module.exports = {
     branches: ['master'],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/npm',
+        [
+            '@semantic-release/git',
+            {
+                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+            },
+        ],
+        '@semantic-release/github',
+    ],
 };
