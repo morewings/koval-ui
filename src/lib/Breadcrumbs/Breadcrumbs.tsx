208d399138323640d97d2e53c88199fa97ce5d30
Dima Vyshniakov <morewings@gmail.com>
2024-04-28 14:08:59 +0200

    feat(component): add Breadcrumbs component
    fix #80

---
+++ src/lib/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,55 @@
+import type {FC, HTMLAttributes, ReactNode, JSX} from 'react';
+import {forwardRef} from 'react';
+import classNames from 'classnames';
+
+import type {DataAttributes, LibraryProps} from '@/internal/LibraryAPI';
+
+import classes from './Breadcrumbs.module.css';
+
+type Item = {
+    name: string;
+    url: string;
+    icon?: FC<HTMLAttributes<HTMLOrSVGElement> & unknown>;
+};
+
+export type Props = DataAttributes &
+    LibraryProps & {
+        children?: ReactNode;
+        items: Item[];
+        showEllipsis?: boolean;
+    };
+
+const conditionallyAddEllipsis = (items: JSX.Element[], showEllipsis: boolean) => {
+    if (showEllipsis) {
+        const [first, ...rest] = items;
+        return [
+            first,
+            <div className={classes.ellipsis} key="ellypsis">
+                &hellip;
+            </div>,
+            ...rest,
+        ];
+    } else {
+        return items;
+    }
+};
+
+export const Breadcrumbs = forwardRef<HTMLDivElement, Props>(
+    ({className, items, showEllipsis = false, ...nativeProps}, ref) => {
+        const children = items.map(({name, url, icon: Icon}) => {
+            return (
+                <a key={name} title={name} className={classes.crumb} href={url}>
+                    {Icon && <Icon className={classes.icon} />}
+                    <span className={classes.text}>{name}</span>
+                </a>
+            );
+        });
+        return (
+            <div {...nativeProps} className={classNames(classes.breadcrumbs, className)} ref={ref}>
+                {conditionallyAddEllipsis(children, showEllipsis)}
+            </div>
+        );
+    }
+);
+
+Breadcrumbs.displayName = 'Breadcrumbs';
