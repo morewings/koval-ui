aad25d73af3ec7ef626627b568a92ca1b0eaa750
Dima Vyshniakov <morewings@gmail.com>
2024-04-28 14:08:59 +0200

    feat(component): add Breadcrumbs component
    fix #80

---
+++ src/lib/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -0,0 +1,57 @@
+import type {Meta, StoryObj} from '@storybook/react';
+// import {fn} from '@storybook/test';
+
+import {CloudUpload} from '@/internal/Icons';
+
+import {Breadcrumbs} from './Breadcrumbs.tsx';
+
+const meta = {
+    title: 'Components/Breadcrumbs',
+    component: Breadcrumbs,
+    parameters: {
+        // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+        layout: 'fullscreen',
+    },
+    args: {
+        showEllipsis: false,
+    },
+    argTypes: {
+        className: {
+            table: {
+                disable: true,
+            },
+        },
+        id: {
+            table: {
+                disable: true,
+            },
+        },
+        role: {
+            table: {
+                disable: true,
+            },
+        },
+    },
+} as Meta<typeof Breadcrumbs>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const itemsMock = [
+    {name: 'Main page', url: 'http://foo', icon: CloudUpload},
+    {name: 'User Settings', url: 'http://foo'},
+    {
+        name: 'Indicate the current page’s location within a navigational hierarchy that automatically adds separators',
+        url: 'http://foo',
+    },
+    {name: 'Profile', url: 'http://foo', icon: CloudUpload},
+];
+
+export const Primary: Story = {
+    render: args => {
+        return <Breadcrumbs {...args} />;
+    },
+    args: {
+        items: itemsMock,
+    },
+};
