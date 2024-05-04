cbf0e0605a02d2e238d66310c75c9427e455bfb3
Dima Vyshniakov <morewings@gmail.com>
2024-05-04 21:48:03 +0200

    refactor(tabs): remove tabs provider

--- src/lib/Tabs/TabButton.tsx
+++ src/lib/Tabs/TabButton.tsx
@@ -3,16 +3,15 @@ import {useCallback} from 'react';
 import classNames from 'classnames';
 
 import classes from './Tabs.module.css';
-import {useActiveTab} from './TabContext.tsx';
 
 type Props = {
     tabName: string;
+    activeName: string;
     onClick: (tabName: string) => void;
     icon?: FC<HTMLAttributes<HTMLOrSVGElement> & unknown>;
 };
 
-export const TabButton: FC<Props> = ({tabName, onClick, icon: Icon}) => {
-    const active = useActiveTab();
+export const TabButton: FC<Props> = ({tabName, onClick, icon: Icon, activeName}) => {
     const handleClick = useCallback(() => {
         onClick(tabName);
     }, [onClick, tabName]);
@@ -21,7 +20,7 @@ export const TabButton: FC<Props> = ({tabName, onClick, icon: Icon}) => {
             type="button"
             key={tabName}
             onClick={handleClick}
-            className={classNames(classes.button, {[classes.selected]: active === tabName})}>
+            className={classNames(classes.button, {[classes.selected]: activeName === tabName})}>
             {Icon && <Icon className={classes.icon} />}
             {tabName}
         </button>
