511e9a3de7d4edcc0e36c138af54a3b4c8f97a4c
Dima Vyshniakov <morewings@gmail.com>
2024-05-28 13:12:27 +0200

    docs(storybook): add viewports from layout component
    fix #190

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,6 +3,44 @@ import {Provider} from './../src/lib/Provider/index.ts';
 
 import 'the-new-css-reset/css/reset.css';
 
+const customViewports = {
+    viewportXS: {
+        name: 'viewport-xs: 320×568',
+        styles: {
+            width: '320px',
+            height: '568px',
+        },
+    },
+    viewportSM: {
+        name: 'viewport-sm: 640×960',
+        styles: {
+            width: '640px',
+            height: '960px',
+        },
+    },
+    viewportMD: {
+        name: 'viewport-md: 992×1194',
+        styles: {
+            width: '992px',
+            height: '1194px',
+        },
+    },
+    viewportLG: {
+        name: 'viewport-lg: 1366×768',
+        styles: {
+            width: '1366px',
+            height: '768px',
+        },
+    },
+    viewportXL: {
+        name: 'viewport-xl: 1920×1080',
+        styles: {
+            width: '1920px',
+            height: '1080px',
+        },
+    },
+};
+
 const preview: Preview = {
     parameters: {
         actions: { argTypesRegex: '^on[A-Z].*' },
@@ -17,6 +55,7 @@ const preview: Preview = {
                 order: ['Intro', 'Layout', 'Inputs', 'Typography', 'Components'],
             },
         },
+        viewport: { viewports: customViewports },
     },
     decorators: [
         (Story) => (
