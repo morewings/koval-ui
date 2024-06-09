6f9d522fd42a9c3e8c49aeebf982348006099127
Dima Vyshniakov <morewings@gmail.com>
2024-06-09 23:25:19 +0200

    fix(carousel): improve auto-rotate behavior
    fix #317

--- src/lib/Carousel/useAutoRotate.ts
+++ src/lib/Carousel/useAutoRotate.ts
@@ -1,4 +1,5 @@
 import type {MutableRefObject} from 'react';
+import {useEffect} from 'react';
 
 import {useInterval} from '@/internal/hooks/useInterval.ts';
 import {useIsInViewport} from '@/internal/hooks/useIsInViewport.tsx';
@@ -6,13 +7,17 @@ import {useDocumentVisible} from '@/internal/hooks/useDocumentVisible.ts';
 
 type Props = {
     rotateFn: () => void;
+    rewindFn: () => void;
     interval?: number | null;
     ref: MutableRefObject<HTMLElement | null>;
 };
 
-export const useAutoRotate = ({rotateFn, interval = null, ref}: Props) => {
+export const useAutoRotate = ({rotateFn, interval = null, ref, rewindFn}: Props) => {
     const isInViewport = useIsInViewport(ref);
     const isActiveTab = useDocumentVisible();
     const isEnabled = Boolean(interval) && isInViewport && isActiveTab;
+    useEffect(() => {
+        !isActiveTab && rewindFn();
+    }, [isActiveTab, rewindFn]);
     useInterval({callback: rotateFn, interval, condition: isEnabled});
 };
