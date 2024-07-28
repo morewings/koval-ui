0839b2c95a260569e37ca5093853ec2ceb359461
Dima Vyshniakov <morewings@gmail.com>
2024-07-28 23:21:15 +0200

    feat(dummy): dummy release

--- release.config.cjs
+++ release.config.cjs
@@ -1,12 +1,13 @@
 /**
  * @type {import('semantic-release').GlobalConfig}
  */
+// eslint-disable-next-line no-undef
 module.exports = {
     branches: ['master'],
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
-        '@semantic-release/npm',
+        // '@semantic-release/npm',
         [
             '@semantic-release/git',
             {
