071d9cc1fa82ed7387959a6227afdf4c21b444ed
Dima Vyshniakov <morewings@gmail.com>
2025-11-02 18:35:59 +0100

    feat(headers): add optional anchor to H1-H6 components
    fix #707

--- src/lib/Text/Headers.tsx
+++ src/lib/Text/Headers.tsx
@@ -3,20 +3,48 @@ import {forwardRef} from 'react';
 import classNames from 'classnames';
 
 import type {DataAttributes, LibraryProps} from '@/internal/LibraryAPI';
+import {IconLink} from '@/internal/Icons';
 
 import classes from './Text.module.css';
 
 export type Props = DataAttributes &
     LibraryProps & {
         children?: ReactNode;
+        /**
+         * Display the anchor link with the Header
+         */
+        anchor?: string;
     };
+
+export const Header = forwardRef<
+    HTMLHeadingElement,
+    Props & {as: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'}
+>(({children, className, as, anchor, ...restProps}, ref) => {
+    const Component = as;
+    return (
+        <Component {...restProps} className={classNames(classes.header, className)} ref={ref}>
+            {children}
+            {anchor && (
+                <a className={classes.anchor} href={`#${anchor}`}>
+                    <IconLink className={classes.icon} />
+                </a>
+            )}
+        </Component>
+    );
+});
+
+Header.displayName = 'Header';
 
 export const H1 = forwardRef<HTMLHeadingElement, Props>(
     ({children, className, ...nativeProps}, ref) => {
         return (
-            <h1 {...nativeProps} className={classNames(classes.header1, className)} ref={ref}>
+            <Header
+                {...nativeProps}
+                as="h1"
+                className={classNames(classes.header1, className)}
+                ref={ref}>
                 {children}
-            </h1>
+            </Header>
         );
     }
 );
@@ -26,9 +54,13 @@ H1.displayName = 'H1';
 export const H2 = forwardRef<HTMLHeadingElement, Props>(
     ({children, className, ...nativeProps}, ref) => {
         return (
-            <h2 {...nativeProps} className={classNames(classes.header2, className)} ref={ref}>
+            <Header
+                {...nativeProps}
+                as="h2"
+                className={classNames(classes.header2, className)}
+                ref={ref}>
                 {children}
-            </h2>
+            </Header>
         );
     }
 );
@@ -38,9 +70,13 @@ H2.displayName = 'H2';
 export const H3 = forwardRef<HTMLHeadingElement, Props>(
     ({children, className, ...nativeProps}, ref) => {
         return (
-            <h3 {...nativeProps} className={classNames(classes.header3, className)} ref={ref}>
+            <Header
+                {...nativeProps}
+                as="h3"
+                className={classNames(classes.header3, className)}
+                ref={ref}>
                 {children}
-            </h3>
+            </Header>
         );
     }
 );
@@ -50,9 +86,13 @@ H3.displayName = 'H3';
 export const H4 = forwardRef<HTMLHeadingElement, Props>(
     ({children, className, ...nativeProps}, ref) => {
         return (
-            <h4 {...nativeProps} className={classNames(classes.header4, className)} ref={ref}>
+            <Header
+                {...nativeProps}
+                as="h4"
+                className={classNames(classes.header4, className)}
+                ref={ref}>
                 {children}
-            </h4>
+            </Header>
         );
     }
 );
@@ -62,9 +102,13 @@ H4.displayName = 'H4';
 export const H5 = forwardRef<HTMLHeadingElement, Props>(
     ({children, className, ...nativeProps}, ref) => {
         return (
-            <h5 {...nativeProps} className={classNames(classes.header5, className)} ref={ref}>
+            <Header
+                {...nativeProps}
+                as="h5"
+                className={classNames(classes.header5, className)}
+                ref={ref}>
                 {children}
-            </h5>
+            </Header>
         );
     }
 );
@@ -74,9 +118,13 @@ H5.displayName = 'H5';
 export const H6 = forwardRef<HTMLHeadingElement, Props>(
     ({children, className, ...nativeProps}, ref) => {
         return (
-            <h6 {...nativeProps} className={classNames(classes.header6, className)} ref={ref}>
+            <Header
+                {...nativeProps}
+                as="h6"
+                className={classNames(classes.header6, className)}
+                ref={ref}>
                 {children}
-            </h6>
+            </Header>
         );
     }
 );
